83f0b305c051930f784bfc79ab6f1e3f2f0f7751
John Terenzio <john@terenz.io>
2021-01-14 09:25:35 -0600

    added python function practice file

---
+++ python_functions.py
@@ -0,0 +1,29 @@
+# Define a function which takes 2 numbers as arguments and returns the absolute
+# value of the difference
+
+# Define a function which takes two strings, first_name and last_name, and
+# returns a string like "Hello <full name>, how are you?"
+
+# You need to implement a function which is part of a public API, it should do
+# the same as above, but the arguments should be in reverse order: last_name,
+# first_name. You should consider calling the function above instead of
+# redefining.
+
+# Define a function which takes a variable number of strings and returns a
+# single string which joins all the arguments with commas. Make sure to reject
+# any arguments that are not strings. How should you reject? Should you ignore
+# or raise an error?
+
+# Define a function which takes a single string and then an optional keyword
+# argument. The keyword argument should be called "capitalize" and if True is
+# passed, it should return the string in all caps. Otherwise, the string that
+# was originally passed should be returned unmodified.
+
+# Define a class called Dog which implements a Dog. The dog shoud be
+# initialized with a name, breed, and sex for the dog. The dog should have
+# methods "bark" which takes an integer between 1 and 4 inclusive for how loud
+# the dog barks. It should return a string about how loud the dog barked. The
+# class should also have "walk" method which takes two tuples of integers that
+# represent x, y coordinates for the start and stop. It should return a string
+# which tells how far the dog walked. Lastly, the dog class should have a to
+# string method which returns a string describing the dog.
